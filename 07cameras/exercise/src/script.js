425c127abcde643de0a2a5c408570334452fdcc9
QuinntonCarter <qdcarter92@protonmail.com>
2022-01-19 12:13:20 -0700

    finished cameras

--- 07cameras/exercise/src/script.js
+++ 07cameras/exercise/src/script.js
@@ -1,17 +1,29 @@
-import './style.css'
-import * as THREE from 'three'
-
-/**
- * Base
- */
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Sizes
 const sizes = {
     width: 800,
     height: 600
 }
+
+// **
+// capture cursor coordinates
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', e => {
+    cursor.x = e.clientX / sizes.width - 0.5
+    cursor.y = -(e.clientY / sizes.height - 0.5)
+})
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
@@ -24,21 +36,41 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-//                                              (fov, aspect ratio, near, far)
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100)
-camera.position.x = 2
-camera.position.y = 2
+//                                           (fov, aspect ratio, near, far)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// ** testing OrthographicCamera **
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(
+//     // (left
+//     -1 * aspectRatio,
+//     // right
+//     1 * aspectRatio,
+//     // top, bottom, near, far)
+//     1, -1, 0.1, 100)
+//     console.log(aspectRatio)
+// **
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// **
+// Controls
+//                                  (camera, DOMelement,)
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 2
+// controls.update()
+
+// **
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
+// **
 // Animate
 const clock = new THREE.Clock()
 
@@ -46,13 +78,25 @@ const animationTick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+    // 1 Update objects
+    // mesh.rotation.y = elapsedTime;
 
-    // Render
+    // 2 update camera
+    // dynamic any angle
+    // camera.position.x = cursor.x * 3
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
+    // move around cube: Math.PI allows half rotation *
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+    controls.update()
+
+    // 3 Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
+    // 4 Call tick again on the next frame
     window.requestAnimationFrame(animationTick)
 }
 
