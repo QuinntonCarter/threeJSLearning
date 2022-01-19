556b80c75798d465ed78a513d97de3092ce9dfdd
QuinntonCarter <qdcarter92@protonmail.com>
2022-01-19 09:06:00 -0700

    learning cameras

--- 07cameras/exercise/src/script.js
+++ 07cameras/exercise/src/script.js
@@ -24,7 +24,9 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+//                                              (fov, aspect ratio, near, far)
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100)
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
@@ -51,7 +53,7 @@ const animationTick = () =>
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    window.requestAnimationFrame(animationTick)
 }
 
 animationTick() 
