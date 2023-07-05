4c1fc703bc2493932ad791a700d8b53f3c6413a7
QuinntonCarter <qdcarter92@protonmail.com>
2023-07-05 17:10:08 -0600

    Finish box gui

--- 21Physics/exercise/src/script.js
+++ 21Physics/exercise/src/script.js
@@ -11,12 +11,22 @@ const gui = new dat.GUI();
 const debugObject = {};
 debugObject.createSphere = () => {
   createSphere(Math.random() * 0.5, {
-    x: Math.random() - 0.5 * 3,
+    x: (Math.random() - 0.5) * 3,
     y: 3,
-    z: Math.random() - 0.5 * 3,
+    z: (Math.random() - 0.5) * 3,
   });
 };
+
+debugObject.createBox = () => {
+  createBox(Math.random(), Math.random(), Math.random(), {
+    x: (Math.random() - 0.5) * 3,
+    y: 3,
+    z: (Math.random() - 0.5) * 3,
+  });
+};
+
 gui.add(debugObject, "createSphere");
+gui.add(debugObject, "createBox");
 
 /**
  * Base
@@ -203,7 +213,7 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const objectsToUpdate = [];
-const sphereGeometry = new THREE.SphereBufferGeometry(radius, 20, 20);
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20);
 const sphereMaterial = new THREE.MeshStandardMaterial({
   metalness: 0.3,
   roughness: 0.4,
@@ -213,6 +223,7 @@ const sphereMaterial = new THREE.MeshStandardMaterial({
 // UTILS
 const createSphere = (radius, position) => {
   // ThreeJS mesh
+
   const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
   mesh.scale.set(radius, radius, radius);
   mesh.castShadow = true;
@@ -236,6 +247,46 @@ const createSphere = (radius, position) => {
   });
 };
 createSphere(0.5, { x: 0, y: 3, z: 0 });
+
+const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+// BAWX
+const createBox = (width, height, depth, position) => {
+  // ThreeJS mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  //   CannonJS body
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  );
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  // save in objects to update
+  objectsToUpdate.push({
+    mesh,
+    body,
+  });
+};
+createBox(Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5, {
+  x: 0,
+  y: 3,
+  z: 0,
+});
 
 /**
  * Animate
@@ -262,6 +313,7 @@ const tick = () => {
 
   for (const object of objectsToUpdate) {
     object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
   }
   // Update controls
   controls.update();
