7fcaa1f7693928feb376503870fea3a917b266d4
QuinntonCarter <qdevsz@gmail.com>
2023-08-10 16:54:33 -0600

    Skipping shaders

--- 27shaderPatterns/exercise/src/shaders/test/fragment.glsl
+++ 27shaderPatterns/exercise/src/shaders/test/fragment.glsl
@@ -1,45 +1,66 @@
-#define PI 3.1415926535897932384626433832795;
+// #define PI 3.1415926535897932384626433832795;
 varying vec2 vUv;
 
 // for random number generation
-float random(vec2 st){
-    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
+// float random(vec2 st){
+//     return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+// }
 
-vec2 rotate(vec2 uv, float rotation, vec2 mid){
-    return vec2(
-      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
-      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
-    );
-}
+// vec2 rotate(vec2 uv, float rotation, vec2 mid){
+//     return vec2(
+//       cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+//       cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+//     );
+// }
 
 void main()
 {
+    // gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+
+    // pattern 1
+    // gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0);
+    // or
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
+    // pattern 1.5
+    // gl_FragColor = vec4(vUv, 0.0, 1.0);
+
+    // pattern 2
+    // gl_FragColor = vec4(vUv, 0.5, 1.0);
+
     // pattern 3
-    // float strength = vUv.x;
+    // gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
+
+    // pattern 3.5
+    // float strength = vUv.x; // used in 3.5 - 4
+    // gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
 
     // pattern 4
-    // float strength = vUv.y;
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
     // pattern 5 invert shading
-    // float strength = 1.0 - vUv.y;
+    // float strength = 1.0 - vUv.y; // used in 5
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
     // pattern 6 invert and foggy gradient
     // float strength = vUv.y * 10.0;
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
     // pattern 7 blides effect; 0 to 100 reset 0 to 100 reset
     // float strength = mod(vUv.y * 10.0, 1.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
     // pattern 8 blides effect; hard edge
     // float strength = mod(vUv.y * 10.0, 1.0);
-    // conditions bad for perfomance
-    // float strength = strength < 0.5 ? strength = 0.0 : strength= 1.0;
-    // strength = step(0.5, strength);
+    // float strength = strength < 0.5 ? strength = 0.0 : strength= 1.0; // bad for perfomance
+    // strength = step(0.5, strength); // better for performance
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
     // pattern 9
     // edit pattern that shows
     // float strength = mod(vUv.y * 10.0, 1.0);
     // strength = step(0.8, strength);
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
+
 
     // pattern 10
     // float strength = mod(vUv.x * 10.0, 1.0);
@@ -144,15 +165,15 @@ void main()
 
     // pattern 32
     // float pi = 3.1415926535897932384626433832795;
-    vec2 rotatedUV = rotate(vUv, (PI * 0.25), vec2(0.5));
+    // vec2 rotatedUV = rotate(vUv, (PI * 0.25), vec2(0.5));
 
-    vec2 lightUVX = vec2(rotatedUV.x * 0.1 + 0.45, rotatedUV.x * 0.5 + 0.25);
-    float lightX = 0.015 / distance(lightUVX, vec2(0.5));
+    // vec2 lightUVX = vec2(rotatedUV.x * 0.1 + 0.45, rotatedUV.x * 0.5 + 0.25);
+    // float lightX = 0.015 / distance(lightUVX, vec2(0.5));
 
-    vec2 lightUVY = vec2(rotatedUV.y * 0.1 + 0.45, rotatedUV.y * 0.5 + 0.25);
-    float lightY = 0.015 / distance(lightUVY, vec2(0.5));
+    // vec2 lightUVY = vec2(rotatedUV.y * 0.1 + 0.45, rotatedUV.y * 0.5 + 0.25);
+    // float lightY = 0.015 / distance(lightUVY, vec2(0.5));
     
-    float strength = lightX * lightY;
+    // float strength = lightX * lightY;
 
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
