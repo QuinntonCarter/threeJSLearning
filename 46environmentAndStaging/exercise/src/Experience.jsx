54840a421639cb1e3be40b8401e3dc7966b2d139
QuinntonCarter <qdevsz@gmail.com>
2023-09-06 22:32:31 -0600

    46 progress sync

--- 46environmentAndStaging/exercise/src/Experience.jsx
+++ 46environmentAndStaging/exercise/src/Experience.jsx
@@ -4,6 +4,8 @@ import {
   useHelper,
   BakeShadows,
   SoftShadows,
+  AccumulativeShadows,
+  RandomizedLight,
 } from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
@@ -15,16 +17,43 @@ export default function Experience() {
   const cube = useRef();
 
   useFrame((state, delta) => {
+    const time = state.clock.elapsedTime;
+    cube.current.position.x = 2 + Math.sin(time);
     cube.current.rotation.y += delta * 0.2;
   });
 
   return (
     <>
       <BakeShadows />
-      {/* <SoftShadows frustum={3.75} size={50} near={9.5} samples={17} /> */}
-      <color args={["ivory"]} attach="background" />;
+      {/* <SoftShadows frustum={3.75} size={50} near={9.5} samples={17} rings={11} /> */}
+      <color
+        args={["ivory"]}
+        attach="background"
+      />
       <Perf position="top-left" />
       <OrbitControls makeDefault />
+      {/* {[x,y (this current value setting prevents z-fighting w shadow and plane),z]} */}
+      <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+      >
+        {/* <directionalLight
+          position={[1, 2, 3]}
+          castShadow
+        /> */}
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={1}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows>
       <directionalLight
         ref={directionalLight}
         position={[1, 2, 3]}
@@ -37,19 +66,27 @@ export default function Experience() {
         shadow-camera-top={5}
         shadow-camera-right={5}
         shadow-camera-bottom={-5}
-        shadow-camera-clock={-5}
+        shadow-camera-left={-5}
       />
       <ambientLight intensity={0.5} />
-      <mesh castShadow position-x={-2}>
+      <mesh
+        castShadow
+        position-x={-2}
+      >
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
-      <mesh castShadow ref={cube} position-x={2} scale={1.5}>
+      <mesh
+        castShadow
+        ref={cube}
+        position-x={2}
+        scale={1.5}
+      >
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
       <mesh
-        // receiveShadow
+        receiveShadow
         position-y={-1}
         rotation-x={-Math.PI * 0.5}
         scale={10}
