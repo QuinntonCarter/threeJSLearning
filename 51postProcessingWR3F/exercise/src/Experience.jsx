6804629daa393f984c2dd9cd46eb7cc1054bf6c0
QuinntonCarter <qdevsz@gmail.com>
2023-09-12 13:28:04 -0600

    Pause 51

--- 51postProcessingWR3F/exercise/src/Experience.jsx
+++ 51postProcessingWR3F/exercise/src/Experience.jsx
@@ -1,11 +1,41 @@
+import { useRef } from "react";
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { EffectComposer } from "@react-three/postprocessing";
+import {
+  Bloom,
+  EffectComposer,
+  Glitch,
+  Noise,
+  Outline,
+  Pixelation,
+  Vignette,
+} from "@react-three/postprocessing";
+import { GlitchMode, BlendFunction } from "postprocessing";
 
 export default function Experience() {
   return (
     <>
-      <EffectComposer disableNormalPass multisampling={0}></EffectComposer>
+      <color args={["#000000"]} attach="background" />
+      <EffectComposer disableNormalPass multisampling={0}>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          //   blendFunction={BlendFunction.COLOR_BURN}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+        {/* <Pixelation granularity={8} /> */}
+        {/* <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.2, 0.04]}
+          //   mode={GlitchMode.CONSTANT_MILD}
+        /> */}
+        {/* <Noise premultiply blendFunction={BlendFunction.OVERLAY} /> */}
+        {/* tone mapping clamps tone range */}
+        {/* mipmapBlur improves blur spread */}
+        {/* luminanceThreshold sets the threshold above which materials will start to glow */}
+        <Bloom mipmapBlur intensity={0.5} luminanceThreshold={0} />
+      </EffectComposer>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
@@ -20,7 +50,14 @@ export default function Experience() {
 
       <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        {/* r, g, b */}
+        {/* <meshStandardMaterial
+          color={[4, 1, 2]}
+          emissive="pink"
+          emissiveIntensity={2}
+          toneMapped={false}
+        /> */}
+        <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} />
       </mesh>
 
       <mesh
